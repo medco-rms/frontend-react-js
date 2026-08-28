a4127f6f54a285faa629b40ecef238fbb523d630
surafelhabte1 <surafelhabte1@gmail.com>
2026-08-29 00:16:44 +0300

    move files to admin folder

--- src/screens/admin/patient/usePatient.tsx
+++ src/screens/admin/patient/usePatient.tsx
@@ -6,7 +6,7 @@ import {
   UserProfileInfo,
   useUtils,
 } from "react-project-scaffold-ts";
-import { type Gender, type Patient } from "../../assets/models";
+import { type Gender, type Patient } from "@/assets/models";
 
 export const usePatient = () => {
   const { GetFormattedIds, ServerDate } = useUtils({});
